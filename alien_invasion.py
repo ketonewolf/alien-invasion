0cfcf30fe604dd99610812165a57e1290e8920eb
David Jones <kalensus@gmail.com>
2025-12-14 19:38:29 +0000

    add optional code for full-screen display mode

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,11 @@ class AlienInvasion:
 
         self.clock = pygame.time.Clock()
         self.settings = Settings()
+
+        # Optional code for running game in full screen mode.
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height)
         )
